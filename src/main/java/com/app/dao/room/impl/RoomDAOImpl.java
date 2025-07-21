062571caad05ed1bb523862a5a394c0fd2173daa
seoungkwon <s2ngkwon2@naver.com>
2025-07-21 16:07:42 +0900

    DB연동 Room 데이터 Insert 저장, Mybatis-config

--- src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
+++ src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
@@ -2,6 +2,8 @@ package com.app.dao.room.impl;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.app.dao.room.RoomDAO;
@@ -13,6 +15,10 @@ import com.app.dto.room.Room;
 @Repository
 public class RoomDAOImpl implements RoomDAO {
 
+	//실질적인 DB 연동 처리 관련 객체
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
 	@Override
 	public List<Room> findRoomList() {
 
@@ -28,7 +34,12 @@ public class RoomDAOImpl implements RoomDAO {
 		
 		//room 매개변수 값 -> DB 저장
 		
-		return 0;
+									//내가 실행할 쿼리가 위치한 식별자
+									// namespace.id
+		int result = sqlSessionTemplate.insert("room_mapper.saveRoom", room);
+		//insert 적용된 행의 숫자가 return 됨.
+		
+		return result;
 	}
 
 }
