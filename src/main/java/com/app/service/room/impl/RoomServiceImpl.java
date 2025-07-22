9a7c5dc2523e2efbfe7d976a173ffbd6f2387800
seoungkwon <s2ngkwon2@naver.com>
2025-07-22 16:00:53 +0900

    관리자페이지 개별 객실 상세 페이지 연결 처리

--- src/main/java/com/app/service/room/impl/RoomServiceImpl.java
+++ src/main/java/com/app/service/room/impl/RoomServiceImpl.java
@@ -54,4 +54,14 @@ public class RoomServiceImpl implements RoomService {
 		return result;
 	}
 
+	@Override
+	public Room findRoomByRoomId(int roomId) {
+		
+		Room room = roomDAO.findRoomByRoomId(roomId);
+		
+		return room;
+	}
+	
+	
+
 }
