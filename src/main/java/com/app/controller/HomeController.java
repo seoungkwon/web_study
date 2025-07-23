244ad622e1aeaeea9821ebee060048a76be26bd7
seoungkwon <s2ngkwon2@naver.com>
2025-07-23 10:27:37 +0900

    사용자 회원가입 페이지 기능 추가 (연습겸용)

--- src/main/java/com/app/controller/HomeController.java
+++ src/main/java/com/app/controller/HomeController.java
@@ -3,18 +3,19 @@ package com.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller	// @ 붙어있으면 어노테이션
+@Controller
 public class HomeController {
 
 	@RequestMapping("/")
+	//@RequestMapping("/")
 	public String home() {
 
-		//return "/WEB-INF/views/home.jsp";
-		//return "/WEB-INF/views/mainpage.jsp";
-		
-		//--ViewResolver 설정 이후
-		
-		// return "mainpage";
+		// return "/WEB-INF/views/home.jsp";
+		// return "/WEB-INF/views/mainpage.jsp";
+
+		// --ViewResolver 설정 이후
+
+		// return "mainpage"
 		return "home";
 	}
 }
--- src/main/java/com/app/controller/study/request/MainController.java
+++ src/main/java/com/app/controller/study/request/MainController.java
@@ -3,7 +3,7 @@ package com.app.controller.study.request;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+//@Controller
 public class MainController {
 
 	@RequestMapping("/main")
