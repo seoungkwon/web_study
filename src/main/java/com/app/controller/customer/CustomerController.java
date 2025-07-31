7004d517843648449f55abb6ef471bf8db8c2e7e
seoungkwon <s2ngkwon2@naver.com>
2025-07-31 10:51:07 +0900

    유효성 검증 2 (DTO 어노테이션 기준)

--- src/main/java/com/app/controller/customer/CustomerController.java
+++ src/main/java/com/app/controller/customer/CustomerController.java
@@ -1,11 +1,17 @@
 package com.app.controller.customer;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -39,7 +45,27 @@ public class CustomerController {
 
 
 	@PostMapping("/customer/signup")
-	public String singupAction(User user) {
+//	public String singupAction(User user) {
+	public String singupAction(@Valid @ModelAttribute User user, BindingResult br) {
+								//유효성검사			검사 결과
+		
+		//검증 결과에 문제가 있느냐 없느냐
+		
+		if(br.hasErrors()) {
+			//문제 내용을 출력
+			List<ObjectError> errorList = br.getAllErrors();
+			for(ObjectError er : errorList) {
+				System.out.println(er.getObjectName());
+				System.out.println(er.getDefaultMessage());
+				System.out.println(er.getCode());
+				System.out.println(er.getCodes()[0]);
+			}
+			
+			//model.addAttribute("user", user);
+			
+			return "customer/signup";
+		}
+		
 
 		//사용자 회원가입 -> 저장 
 		int result = userService.saveCustomerUser(user);
@@ -81,7 +107,7 @@ public class CustomerController {
 		System.out.println(userDupCheck);
 
 		log.info("checkDupIdJson 아이디 중복체크 : {}", userDupCheck);
-		
+
 		//처리 로직
 		//클라이언트에서 보낸 데이터가 중복된 사용자 아이디인지 체크
 		boolean result = userService.isDuplicatedId(userDupCheck.getId());
